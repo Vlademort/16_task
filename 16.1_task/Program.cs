3caae124cce8cb11241809670ddb054094735250
Владислав Митьков <vladeemitkov96@gmail.com>
2022-01-30 17:58:10 +0300

    Добавьте файлы проекта.

---
+++ 16.1_task/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+
+namespace _16._1_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            Product[] product_array = new Product[N];
+            
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"Товар № {i+1}");
+                Console.Write("Введите код товара: ");
+                int code = Convert.ToInt32(Console.ReadLine());                
+                Console.Write("Введите наименование товара: ");
+                string name = Convert.ToString(Console.ReadLine());                
+                Console.Write("Введите стоимость товара: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+                Product product = new Product(code, name, price);
+                product_array[i] = product;                
+            }
+
+            JsonSerializerOptions option = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string path = "D:/Products.json";            
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }            
+                        
+            for (int i = 0; i < N; i++)
+            {
+                string json_string = JsonSerializer.Serialize(product_array[i], option);                
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine(json_string);                   
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Product
+    {
+        [JsonPropertyName("Код товара")]
+        public int CodeProduct { get; set; }
+
+        [JsonPropertyName("Наименование товара")]
+        public string NameProduct { get; set; }
+
+        [JsonPropertyName("Цена товара")]
+        public double PriceProduct { get; set; }
+
+       public Product (int code, string name, double price)
+        {
+            CodeProduct = code;
+            NameProduct = name;
+            PriceProduct = price;
+        }
+
+    }
+}
---
+++ 16.2_task/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+
+namespace _16._2_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json_string = "";
+            int count = 0;
+            double max_price = 0;
+            int index_max_price = 0;
+            string path = "D:/Products.json";
+            
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                json_string = sr.ReadToEnd();
+            }
+
+            string my_string = json_string.Replace("{", "\t{");
+            string[] product_array = my_string.Split("\t", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var sub in product_array)
+            {
+                //Console.WriteLine($"Подстрока\n{sub}");
+                count += 1;
+            }
+
+
+            Product[] path_product = new Product[count];
+            JsonSerializerOptions option = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            for (int i = 0; i < count; i++)
+            {
+                path_product[i] = JsonSerializer.Deserialize<Product>(product_array[i], option);
+                if (max_price < path_product[i].PriceProduct)
+                {
+                    max_price = path_product[i].PriceProduct;
+                    index_max_price = i;
+                }
+            }
+
+            Console.WriteLine($"{path_product[index_max_price].NameProduct} - самый дорогой товар");
+            Console.WriteLine("{0:f2} - его цена", path_product[index_max_price].PriceProduct);
+            Console.ReadKey();
+
+        }
+    }
+
+    class Product
+    {
+        [JsonPropertyName("Код товара")]
+        public int CodeProduct { get; set; }
+
+        [JsonPropertyName("Наименование товара")]
+        public string NameProduct { get; set; }
+
+        [JsonPropertyName("Цена товара")]
+        public double PriceProduct { get; set; }        
+
+    }
+}
